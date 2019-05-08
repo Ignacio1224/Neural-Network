6070c6a1a85fd606a2dfc9a280ba250a0c50ccc3
Ignacio <ignaciocabrera1224@gmail.com>
2019-05-08 15:15:18 -0300

    Matrix constructor Matrix add Martrix checkDimensions Matrix dot Matrix multiply Matrix randomWeights Matrix subtract Matrix transpose Matrix Getters & Setters

--- js/Init.js
+++ js/Init.js
@@ -1,11 +1,17 @@
-let m1 = new Matrix(2, 2);
-let m2 = new Matrix(2, 2, [
-    [1, 2],
-    [3, 4]
+
+let m1 = new Matrix(2, 3, [
+    [1, 2, 3],
+    [4, 5, 6]
+]);
+let m2 = new Matrix(3, 2, [
+    [7, 8],
+    [9, 10],
+    [11, 12]
 ]);
 
 
-m1.randomWeights();
 console.table(m1.data);
-m2 = Matrix.add(m1, m2);
 console.table(m2.data);
+m2 = Matrix.dot(m1, m2);
+console.table(m2.data);
+
--- js/Matrix.js
+++ js/Matrix.js
@@ -5,7 +5,7 @@ class Matrix {
         this._cols = cols;
         this._data = data;
 
-        // Initialize with zeroes if no data
+        // Initialize data with zeroes if no data
         if (data == null || data.length == 0) {
             this._data = [];
             for (let i = 0; i < this._rows; i++) {
@@ -24,11 +24,64 @@ class Matrix {
     }
 
 
+    // Add two matrices
+    static add (m1, m2) {
+        Matrix.checkDimensions (m1, m2);
 
+        let s = new Matrix (m1.rows, m2.cols);
+
+        for (let i = 0; i < m1.rows; i++) {
+            for (let j = 0; j < m1.cols; j++) {
+                s.data[i][j] = m1.data[i][j] + m2.data[i][j];
+            }
+        }
+
+        return s;
+    }
+
+    // Check the dimensions of both matrices to operate with them
     static checkDimensions (m1, m2) {
         if (m1.rows != m2.rows | m1.cols != m2.cols) {
             throw new Error ("The numbers of rows are diferent!");
         }
+    }
+
+    // Multiply two crossed matrices
+    static dot (m1, m2) {
+        if (m1.cols != m2.rows) {
+            throw new Error ("Error! The matrices are not 'dot' compatible");
+        }
+
+        let s = new Matrix (m1.rows, m2.cols);
+
+        for (let i = 0; i < s.rows; i++) {
+            for (let j = 0; j < s.cols; j++) {
+                let sum = 0;
+                
+                for (let k = 0; k < m1.cols; k++) {
+                    sum += m1.data[i][k] * m2.data[k][j];
+                }
+                
+                s.data[i][j] = sum;
+            }
+        }
+
+        return s;
+    }
+
+    // Multiply two matrices
+    static multiply (m1, m2) {
+        Matrix.checkDimensions (m1, m2);
+
+        let s = new Matrix (m1.rows, m1.cols);
+
+        for (let i = 0; i < s.rows; i++) {
+            for (let j = 0; j < s.cols; j++) {
+                s.data[i][j] = m1.data[i][j] * m2.data[i][j];
+            }
+        }
+
+        return s;
     }
 
     // Set random weights between -1 & 1
@@ -40,14 +93,28 @@ class Matrix {
         }
     }
 
-    static add (m1, m2) {
+    // Subtract two matrices
+    static subtract (m1, m2) {
         Matrix.checkDimensions (m1, m2);
 
-        let s = new Matrix (m1.rows, m2.cols);
+        let s = new Matrix (m1.rows, m1.cols);
 
-        for (let i = 0; i < m1.rows; i++) {
-            for (let j = 0; j < m1.cols; j++) {
-                s.data[i][j] = m1.data[i][j] + m2.data[i][j];
+        for (let i = 0; i < s.rows; i++) {
+            for (let j = 0; j < s.cols; j++) {
+                s.data[i][j] = m1.data[i][j] - m2.data[i][j];
+            }
+        }
+
+        return s;
+    }
+
+    // Transpose a matrix
+    static transpose (m1) {
+        let s = new Matrix (m1.cols, m1.rows);
+
+        for (let i = 0; i < s.rows; i++) {
+            for (let j = 0; j < s.cols; j++) {
+                s.data[i][j] = m1.data[j][i];
             }
         }
 
@@ -66,4 +133,6 @@ class Matrix {
     get data () {
         return this._data;
     }
+
 }
+
