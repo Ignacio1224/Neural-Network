ddf00eb8dcf5215cb23524c822a2730f447f2540
Ignacio1224 <ignaciocabrera1224@gmail.com>
2019-05-10 13:52:29 -0300

    Continued with draws.... Check the positions

--- js/Draw.js
+++ js/Draw.js
@@ -1,57 +1,17 @@
 // Draw Neural Network
 
 
-// Get the used Neural Network.
-const NN = NEURAL_NETWORK;
 
-const CANVAS_HEIGHT = 720; // Height of canvas in pixels.
-const CANVAS_WIDTH = 1024; // Width of canvas in pixels.
+const CANVAS_DIMENSIONS = {
+    w: 1024,
+    h: 720
+}; // Size of canvas.
 
-const FULL_DIVISION_PER_LAYER = CANVAS_WIDTH / NN.num_layers; // Size of LayerD (included edges).
-
-const K_P_LD = FULL_DIVISION_PER_LAYER / 3; // Constant of proportionality in LayerD.
-let LAYERD_DIMENTIONS = {
-    w: K_P_LD * 2, // Width
-    y: 0
-};
-const LAYERD_DRAW = {
-    border_color: '#000000', // Color of borders' shape.
-    line_width: 2 // Line width in pixels.
-};
-let LAYERD_OFFSET = {
-    x: K_P_LD / 2,
-    y: 0
-}; // Distance in pixels between LayersD edge and LyersD.pos in x direction.
-const LAYERD_POS_INIT = {
-    x: 0,
-    y: 0
-}; // Cartitian coordinates of layer in pixels.
-
-const K_P_ND = LAYERD_DIMENTIONS.w / 3; // Constant of proportionality in LayerD.
-const NEUROND_DRAWS = {
-    border_color: '#FF00FF', // Color of borders' shape.
-    fill_color: '#FF00FF', // Color of shape.
-    line_width: 2 // Line width in pixels.
-};
-let NEURON_DIMENTIONS = {
-    w: K_P_ND * 2,
-    h: 0
-};
-const NEUROND_OFFSET = {
-    x: K_P_ND / 2,
-    y: 20
-}; // Distance in pixels between NeuronsD edge and NeuronD.pos in x direction.
-const NEUROND_POS_INIT = {
-    x: 0,
-    y: 0
-}; // Cartitian coordinates of neuron in pixels.
-
-const NEUROND_RAD = NEURON_DIMENTIONS.w / 2; // Radious of neuron in pixels.
 
 function drawNeuralNetwork() {
 
     // Create canvas & add to dom.
-    let canvas = create(`<canvas id='canvas' height='${CANVAS_HEIGHT}' width='${CANVAS_WIDTH}'></canvas>`);
+    let canvas = create(`<canvas id='canvas' width='${CANVAS_DIMENSIONS.w}' height='${CANVAS_DIMENSIONS.h}'></canvas>`);
     document.body.insertBefore(canvas, document.body.childNodes[0]);
 
     // Create canvas_pbject.
@@ -63,71 +23,65 @@ function drawNeuralNetwork() {
     // Get the context of canvas.
     let ctx = canvas_object.getContext('2d');
 
-    let nn_d = new NeuralNetworkD([]);
+    let nn_d = new NeuralNetworkD(CANVAS_DIMENSIONS, NEURAL_NETWORK);
 
-    console.log("------------DIMENSIONS------------");
-    
-    console.log("Full layer width: " + FULL_DIVISION_PER_LAYER);
-    console.log("Layer width: " + LAYERD_DIMENTIONS.w);
-    console.log("Layer offset x (one side): " + LAYERD_OFFSET.x);
+    // console.log("------------DIMENSIONS------------\n\n");
 
-    console.log("Full neuron width: " + LAYERD_DIMENTIONS.w);
-    console.log("Neuron width: " + NEURON_DIMENTIONS.w);
-    console.log("Neuron offset x (one side): " + NEUROND_OFFSET.x);
-    console.log("Neuron radious: " + NEUROND_RAD);
-    
-    // Add neurons to layers
-    for (let i = 0; i < NN.num_layers; i++) {
+    // console.log("Full layer width: " + FULL_DIVISION_PER_LAYER);
+    // console.log("\nLayer width: " + LAYERD_DIMENTIONS.w);
+    // console.log("\nLayer offset x (one side): " + LAYERD_OFFSET.x);
+
+    // console.log("\nFull neuron width: " + LAYERD_DIMENTIONS.w);
+    // console.log("\nNeuron width: " + NEURON_DIMENTIONS.w);
+    // console.log("\nNeuron offset x (one side): " + NEUROND_OFFSET.x);
+    // console.log("\nNeuron radious: " + NEUROND_RAD);
+
+    // Add neurons to layers    
+    for (let i = 0; i < nn_d.numLayersD; i++) {
 
         let l;
-        if (i > 0) {
+        if (i == 0) {
+            l = new LayerD(nn_d.partSize);
+
+        } else {
             const prev_l = nn_d.getLayerD(i - 1);
-            const x = prev_l.pos.x + LAYERD_DIMENTIONS.w + LAYERD_OFFSET.x;
+            const x = prev_l.pos.x + nn_d.partSize.w + prev_l.offset.x;
             const y = prev_l.pos.y;
 
-            l = new LayerD({
+            l = new LayerD(nn_d.partSize, {
                 x,
                 y
-            }, []);
-        } else {
-            l = new LayerD({
-                    x: LAYERD_POS_INIT.x + LAYERD_OFFSET.x * 2,
-                    y: LAYERD_POS_INIT.y + LAYERD_OFFSET.y,
-                },
-                []);
+            });
         }
-        console.log(l);
 
-        for (let j = 0; j < NN.layers[i].length; j++) {
+        for (let j = 0; j < nn_d.u_layers_d[i].length; j++) {
             let n;
-            if (j > 0) {
-                const perv_n = l.getNeuronD(j - 1);
-                const x = l.pos.x + NEUROND_OFFSET.x + NEUROND_RAD;
-                const y = perv_n.pos.y + NEUROND_RAD * 2 + NEUROND_OFFSET.y;
+            if (j == 0) {
+                n = new NeuronD(l.partialSize);
 
-                n = new NeuronD({
+            } else {
+                const perv_n = l.getNeuronD(j - 1);
+                const x = perv_n.offset.x + perv_n.r;
+                const y = perv_n.pos.y + perv_n.r * 2 + perv_n.offset.y;
+
+                n = new NeuronD(l.partialSize, {
                     x,
                     y
-                }, NEUROND_RAD);
-            } else {
-                n = new NeuronD({
-                    x: l.pos.x + NEUROND_OFFSET.x + NEUROND_RAD,
-                    y: 0
-                }, NEUROND_RAD);
+                });
             }
 
             l.addNeuronD(n);
         }
 
+        console.log(l);
+
         nn_d.addLayerD(l);
 
     }
 
-    // for (const l of nn_d.layers_d) {
-    //     l.draw(ctx);
-    // }
-
-    // ctx.moveTo(0, 0);
+    for (const l of nn_d.layers_d) {
+        l.draw(ctx);
+    }
 
     // for (const l of nn_d.layers_d) {
     //     for (const n of l.neurons) {
@@ -150,9 +104,47 @@ function create(htmlStr) {
 
 
 class LayerD {
-    constructor(pos, neurons) {
-        this._pos = pos;
+
+    constructor(full_size, pos, neurons = []) {
+
+        this._full_size = full_size; // Size of of layer.
+
         this._neurons = neurons;
+
+        this._proportionality = {
+            x: 3,
+            y: 3
+        }; // Divisions of layer.
+
+        this._part_size = {
+            w: this._full_size.w / this._proportionality.x,
+            h: this._full_size.h / this._proportionality.y
+        }; // Size of each part.
+
+        this._offset = {
+            x: this._part_size.w / 2,
+            y: this._part_size.h / 2
+        }; // Offset of layer.
+
+        this._partial_size = {
+            w: this._part_size.w * (this._proportionality.x - 1),
+            h: this._part_size.h * (this._proportionality.y - 1)
+        }; // Size to neuron.
+
+
+        this._pos = pos ? {
+            x: pos.x,
+            y: pos.y
+        } : {
+            x: this._offset.x,
+            y: this._offset.y
+        }; // Position on cartesian plane.
+
+        this._style = {
+            border_color: '#000000',
+            line_width: 2
+        }; // Style to draw the layer.
+
     }
 
 
@@ -171,8 +163,8 @@ class LayerD {
      * @description Draws the LayerD
      */
     draw(ctx) {
-        const fw = this.fullWidth();
-        const fh = this.fullHeight();
+        const fw = this._part_size.w;
+        const fh = this._part_size.h;
         const line_x_large = Math.floor(fw / 5);
         let x = this._pos.x;
         let y = this._pos.y;
@@ -188,47 +180,18 @@ class LayerD {
 
         console.log(x);
 
-        x = x + line_x_large + LAYERD_OFFSET.x;
+        x = x + line_x_large + this._offset.x;
 
-        // ctx.moveTo(x + line_x_large + LAYERD_OFFSET.x, y);
-        // ctx.lineTo(x + line_x_large + LAYERD_OFFSET.x + line_x_large, y);
+        ctx.moveTo(x, this._pos.y);
+        ctx.lineTo(x + line_x_large, this._pos.y);
 
+        ctx.moveTo(x, this._pos.y + fh);
+        ctx.lineTo(x + line_x_large, this._pos.y + fh);
 
-        // ctx.moveTo(this._pos.x + line_x_large + LAYERD_OFFSET.x, this._pos.y + this.fullHeight());
-        // ctx.lineTo(this._pos.x + line_x_large + LAYERD_OFFSET.x + line_x_large, this._pos.y + this.fullHeight());
-
-        // ctx.moveTo(this._pos.x + line_x_large + LAYERD_OFFSET.x + line_x_large, this._pos.y);
-        // ctx.lineTo(this._pos.x + line_x_large + LAYERD_OFFSET.x + line_x_large, this._pos.y + this.fullHeight());
+        ctx.moveTo(x + line_x_large, this._pos.y);
+        ctx.lineTo(x + line_x_large, this._pos.y + fh);
 
         ctx.stroke();
-    }
-
-    /**
-     * fullHeight
-     * @returns {Number}
-     * @description Returns the sum of al radious of neurons plus 12 pixels;
-     */
-    fullHeight() {
-        let h = 0;
-        for (const n of this._neurons) {
-            h += n.r * 2;
-        }
-        h += NEUROND_OFFSET.y;
-        h += 12;
-        return h;
-    }
-
-    /**
-     * fullWidth
-     * @returns {Number}
-     * @description Returns the sum of al radious of neurons plus 12 pixels;
-     */
-    fullWidth() {
-        let w = 0;
-        for (const n of this._neurons) {
-            w += n.r * 2;
-        }
-        return w + LAYERD_OFFSET.y * 2;
     }
 
     /**
@@ -243,12 +206,29 @@ class LayerD {
 
 
     /* Getters & Setters */
-    get pos() {
-        return this._pos;
+    get fullSize() {
+        return this._full_size;
     }
 
     get neurons() {
         return this._neurons;
+    }
+
+    get offset() {
+        return this._offset;
+    }
+
+    get partialSize() {
+        return this._part_size;
+    }
+
+    get pos() {
+        return this._pos;
+    }
+
+
+    set neurons(neurons) {
+        this._neurons = neurons;
     }
 
     set pos(pos) {
@@ -256,15 +236,30 @@ class LayerD {
         this._pos.y = pos.y;
     }
 
-    set neurons(neurons) {
-        this._neurons = neurons;
-    }
-
 }
 
 class NeuralNetworkD {
-    constructor(layers_d) {
-        this._layers_d = layers_d;
+
+    constructor(full_size, used_neural_network) {
+
+        this._layers_d = []; // Phisic layers.
+
+        this._u_layers_d = used_neural_network.layers; // Logic layers.
+
+        this._num_u_layers_d = this._u_layers_d.length; // Number of layers.
+
+        this._full_size = full_size; // Full size of neural network.
+
+        this._proportionality = {
+            x: this._num_u_layers_d,
+            y: 3
+        }; // Divisions of layer.
+
+        this._part_size = {
+            w: this._full_size.w / this._proportionality.x,
+            h: this._full_size.h / this._proportionality.y
+        }; // Size of each layer.
+
     }
 
 
@@ -289,19 +284,75 @@ class NeuralNetworkD {
 
 
     /* Getters & Setters */
+    get fullSize() {
+        return this._full_size;
+    }
+
     get layers_d() {
         return this._layers_d;
     }
 
+    get u_layers_d() {
+        return this._u_layers_d;
+    }
+
+    get numLayersD() {
+        return this._num_u_layers_d;
+    }
+
+    get partSize() {
+        return this._part_size;
+    }
+
+
     set layers_d(layers_d) {
         this._layers_d = layers_d;
     }
+
 }
 
 class NeuronD {
-    constructor(pos, r) {
-        this._pos = pos;
-        this._r = r;
+
+    constructor(full_size, pos = null) {
+
+        this._proportionality = {
+            x: 3,
+            y: 3
+        }; // Divisions of layer.
+
+        this._full_size = full_size // Size of of layer.
+
+        this._part_size = {
+            w: this._full_size.w / this._proportionality.x,
+            h: this._full_size.h / this._proportionality.y
+        }; // Size of each part.
+
+        this._partial_size = {
+            w: this._part_size.w * (this._proportionality.x - 1),
+            h: this._part_size.h * (this._proportionality.y - 1)
+        }; // Size to neuron.
+
+        this._offset = {
+            x: this._part_size.w / 2,
+            y: this._part_size.h / 2
+        }; // Offset of layer.
+
+        this._r = this._partial_size.w / 2; // Radious of neuron.
+
+        this._pos = pos ? {
+            x: pos.x,
+            y: pos.y
+        } : {
+            x: this._offset.x + this._r,
+            y: this._offset.y
+        }; // Position on cartesian plane.
+
+        this._style = {
+            border_color: '#FF00FF',
+            fill_color: '#FF00FF',
+            line_width: 2
+        }; // Style to draw the neuron.
+
     }
 
 
@@ -311,9 +362,9 @@ class NeuronD {
      * @description Draws the NeuronD on ctx
      */
     draw(ctx) {
-        ctx.lineWidth = NEUROND_DRAWS.line_width;
-        ctx.strokeStyle = NEUROND_DRAWS.border_color;
-        ctx.fillStyle = NEUROND_DRAWS.fill_color;
+        ctx.lineWidth = this._style.line_width;
+        ctx.strokeStyle = this._style.border_color;
+        ctx.fillStyle = this._style.fill_color;
         ctx.beginPath();
         ctx.arc(this._pos.x, this._pos.y, this._r, 0, 2 * Math.PI);
         ctx.stroke();
@@ -321,6 +372,18 @@ class NeuronD {
 
 
     /* Getters & Setters */
+    get fullSize() {
+        return this._full_size;
+    }
+
+    get partialSize() {
+        return this._part_size;
+    }
+
+    get offset() {
+        return this._offset;
+    }
+
     get pos() {
         return this._pos;
     }
@@ -329,12 +392,10 @@ class NeuronD {
         return this._r
     }
 
+
     set pos(pos) {
         this._pos.x = pos.x;
         this._pos.y = pos.y;
     }
 
-    set r(r) {
-        this._r = r;
-    }
 }
